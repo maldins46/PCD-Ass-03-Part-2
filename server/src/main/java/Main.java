9ac4e91788a0b93acb8c7b75c5509beb413c6fb1
riccardomaldini <riccardo.maldini@gmail.com>
2020-06-16 17:09:52 +0200

    New docker system for server, example of message send-receive

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -1,5 +1,47 @@
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.DeliverCallback;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.TimeoutException;
+
 public class Main {
+    private final static String QUEUE_NAME = "hello";
+
     public static void main(String[] args) {
         System.out.println("Server ready!");
+
+        ConnectionFactory factory = new ConnectionFactory();
+        // todo only in prod factory.setHost("rabbit");
+        factory.setHost("localhost");
+
+
+        try {
+            Connection connection = null;
+            connection = factory.newConnection();
+            Channel channel = connection.createChannel();
+
+            // test send message
+            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+            String message = "Hello World!";
+            channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
+            System.out.println(" [x] Sent '" + message + "'");
+
+            // test recieve callback
+            DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+                String receivedMessage = new String(delivery.getBody(), StandardCharsets.UTF_8);
+                System.out.println(" [x] Received '" + receivedMessage + "'");
+            };
+            channel.basicConsume(QUEUE_NAME, true, deliverCallback, consumerTag -> { });
+
+            System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (TimeoutException e) {
+            e.printStackTrace();
+        }
     }
 }
