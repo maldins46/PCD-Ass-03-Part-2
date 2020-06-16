4fd8ded8485ebfa02ce14add56a154178dc4f766
riccardomaldini <riccardo.maldini@gmail.com>
2020-06-16 17:39:52 +0200

    Added gson for message serializing

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -1,7 +1,9 @@
+import com.google.gson.Gson;
 import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.DeliverCallback;
+import messages.NewPlayerMsg;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
@@ -25,13 +27,27 @@ public class Main {
 
             // test send message
             channel.queueDeclare(QUEUE_NAME, false, false, false, null);
-            String message = "Hello World!";
+            Gson gson = new Gson();
+            NewPlayerMsg msg = new NewPlayerMsg();
+            String message = gson.toJson(msg);
             channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
             System.out.println(" [x] Sent '" + message + "'");
 
             // test recieve callback
             DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+                Gson gson2 = new Gson();
                 String receivedMessage = new String(delivery.getBody(), StandardCharsets.UTF_8);
+                switch ((String) delivery.getProperties().getHeaders().get("type")) {
+                    case "playerSubscription": {
+                        NewPlayerMsg newPlayerMsg = gson2.fromJson(receivedMessage, NewPlayerMsg.class);
+                        break;
+                    }
+
+                    default:
+
+                        break;
+                }
+
                 System.out.println(" [x] Received '" + receivedMessage + "'");
             };
             channel.basicConsume(QUEUE_NAME, true, deliverCallback, consumerTag -> { });
---
+++ server/src/main/java/messages/NewPlayerMsg.java
@@ -0,0 +1,13 @@
+package messages;
+
+public class NewPlayerMsg {
+    private String playerName;
+
+    public void setPlayerName(String playerName) {
+        this.playerName = playerName;
+    }
+
+    public String getPlayerName() {
+        return playerName;
+    }
+}
