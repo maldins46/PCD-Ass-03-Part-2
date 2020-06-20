11462a88452074a3bdd066329c1d9969091d83f9
Thomas Angelini <thomas.angelini@studio.unibo.it>
2020-06-20 15:50:24 +0200

    Comment on these classes

--- server/src/main/java/common/client/config/Hosts.java
+++ server/src/main/java/common/client/config/Hosts.java
@@ -1,12 +1,19 @@
 package common.client.config;
 
+/**
+ * Server host name.
+ */
 public final class Hosts {
     public static final String LOCAL = "localhost";
     public static final String INTERNAL_RABBIT = "rabbit";
 
     private Hosts() { }
 
-
+    /**
+     * Responds if a host is known.
+     * @param host The host to consider.
+     * @return True if is known, false otherwise.
+     */
     public static boolean isKnownHost(final String host) {
         return LOCAL.equals(host) || INTERNAL_RABBIT.equals(host);
     }
--- server/src/main/java/common/model/Tile.java
+++ server/src/main/java/common/model/Tile.java
@@ -1,42 +1,90 @@
 package common.model;
 
+
 /**
- * todo
+ * Tile of the puzzle.
  */
-public class Tile {
+public final class Tile {
+
+    /**
+     * The right position of the tile.
+     */
     private int originalPosition;
+
+
+    /**
+     * Current position.
+     */
     private int currentPosition;
 
-    // se null, nessun giocatore ha selezionato la casella
+    /**
+     * Takes name of player that selected this element. Null if anyone have
+     * selected this tile.
+     */
     private String selectorPlayer;
 
 
+    /**
+     * Create a tile and set original and current positions to the original
+     * position.
+     * @param originalPosition The original position.
+     */
     public Tile(final int originalPosition) {
         this.originalPosition = originalPosition;
         this.currentPosition = originalPosition;
     }
 
+
+    /**
+     * Getter for original position.
+     * @return the original position.
+     */
     public int getOriginalPosition() {
         return originalPosition;
     }
 
-    public void setOriginalPosition(int originalPosition) {
+
+    /**
+     * Setter for the original position.
+     * @param originalPosition the right position.
+     */
+    public void setOriginalPosition(final int originalPosition) {
         this.originalPosition = originalPosition;
     }
 
+
+    /**
+     * Getter for current position.
+     * @return the current position.
+     */
     public int getCurrentPosition() {
         return currentPosition;
     }
 
-    public void setCurrentPosition(int currentPosition) {
+
+    /**
+     * Setter for the original position.
+     * @param currentPosition the current position.
+     */
+    public void setCurrentPosition(final int currentPosition) {
         this.currentPosition = currentPosition;
     }
 
+
+    /**
+     * Getter for selector player.
+     * @return the selector player is present.
+     */
     public String getSelectorPlayer() {
         return selectorPlayer;
     }
 
-    public void setSelectorPlayer(String selectorPlayer) {
+
+    /**
+     * Setter for the selector player.
+     * @param selectorPlayer the selector player.
+     */
+    public void setSelectorPlayer(final String selectorPlayer) {
         this.selectorPlayer = selectorPlayer;
     }
 }
