afc7d4460590846e2667e7b26b9c3e148c3984ac
riccardomaldini <riccardo.maldini@gmail.com>
2020-06-20 12:06:12 +0200

    Added some logs

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -2,25 +2,35 @@ import client.GameClient;
 import client.config.Hosts;
 import model.GameData;
 import callbacks.Callbacks;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Main {
     private static GameData gameData;
     private static GameClient gameClient;
 
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
+
 
     public static void main(final String[] args) {
-        System.out.println("Puzzle server started!");
+
+        logger.info("Assignment 03, pt.2 - Maldini, Gorini, Angelini - MultiPlayer Puzzle Server");
+        logger.info("Server started...");
+
+        final String host = Hosts.LOCAL;
 
         gameData = new GameData();
         gameClient = new GameClient.Builder()
-                .addHost(Hosts.LOCAL)
+                .addHost(host)
                 .addCallback(Callbacks.newPlayerMsg(gameClient, gameData))
                 .addCallback(Callbacks.selectMsg(gameClient, gameData))
                 .addCallback(Callbacks.swapRequestMsg(gameClient, gameData))
                 .addCallback(Callbacks.rematchMsg(gameClient, gameData))
                 .build();
 
+        logger.info("Connecting to the RabbitMQ Broker with host " + host + "...");
         gameClient.connect();
-        System.out.println("Sever ready, waiting for messages");
+        logger.info("✓ Server ready, waiting for messages");
     }
 }
