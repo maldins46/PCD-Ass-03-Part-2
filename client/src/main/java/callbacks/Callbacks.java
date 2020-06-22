7d89f931839526f036fb30221d821597da16844e
Thomas Angelini <thomas.angelini@studio.unibo.it>
2020-06-22 10:42:30 +0200

    Comment to the class.

--- client/src/main/java/callbacks/Callbacks.java
+++ client/src/main/java/callbacks/Callbacks.java
@@ -2,16 +2,24 @@ package callbacks;
 
 import common.client.CtxCallback;
 import common.client.GameClient;
-import common.gameState.ModifiableGameState;
-import common.gameState.ReadableGameState;
 import common.gameState.ReplaceableGameState;
 import gui.PuzzleGui;
 
+/**
+ * Factory for callbacks.
+ */
 public final class Callbacks {
+
     private Callbacks() { }
 
+
     /**
-     * todo
+     * Create a GameState Callback. This callback handle a Msg that define the
+     * updated structure of puzzle.
+     * @param client The client that have to receive this message.
+     * @param state The game state for client.
+     * @param gui The graphic interface.
+     * @return The GameStateCallbacks.
      */
     public static CtxCallback gameStateMsg(final GameClient client, final ReplaceableGameState state, final PuzzleGui gui) {
         return new GameStateCallback(client, state, gui);
@@ -19,7 +27,12 @@ public final class Callbacks {
 
 
     /**
-     * todo
+     * Create a AckMsg Callback. This callback handles AckMsg.
+     * This msg confirms an action of the player.
+     * @param client The client that have to receive this message.
+     * @param state The game state for client.
+     * @param gui The graphic interface.
+     * @return The AckCallback.
      */
     public static CtxCallback ackMsg(final GameClient client, final ReplaceableGameState state, final PuzzleGui gui) {
         return new AckCallback(client, state, gui);
